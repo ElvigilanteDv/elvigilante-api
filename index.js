120b4ec84ca2c5accf7ae6fd08e57bf92aa5726b
El vigilante <cololacalempira5@gmail.com>
2026-06-08 21:47:57 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ const toolQr = require('./routes/tools/qrcode');
 const toolSsweb = require('./routes/tools/ssweb');
 const searchPin = require('./routes/search/pinterest');
 const searchTt = require('./routes/search/tiktok');
+const searchYt = require('./routes/search/youtube'); // 👈 NUEVO
 const dlFb = require('./routes/download/facebookvid');
 const dlIg = require('./routes/download/instagramvid');
 const dlTw = require('./routes/download/twitter');
@@ -34,6 +35,22 @@ const dlTt = require('./routes/download/tiktok');
 const dlYtAudio = require('./routes/download/ytaudio');
 const dlYtVideo = require('./routes/download/ytvideo');
 const userAuth = require('./routes/users');
+
+app.use('/api/auth', userAuth);
+
+app.use('/api/ai/gemini', authHandler, aiGemini);
+app.use('/api/tools/qr', authHandler, toolQr);
+app.use('/api/tools/ssweb', authHandler, toolSsweb);
+app.use('/api/search/pinterest', authHandler, searchPin);
+app.use('/api/search/tiktok', authHandler, searchTt);
+app.use('/api/search/youtube', authHandler, searchYt); // 👈 NUEVO
+app.use('/api/download/facebook', authHandler, dlFb);
+app.use('/api/download/instagram', authHandler, dlIg);
+app.use('/api/download/twitter', authHandler, dlTw);
+app.use('/api/download/pinterest', authHandler, dlPin);
+app.use('/api/download/tiktok', authHandler, dlTt);
+app.use('/api/download/ytaudio', authHandler, dlYtAudio);
+app.use('/api/download/ytvideo', authHandler, dlYtVideo);
 
 app.use('/api/auth', userAuth);
 
