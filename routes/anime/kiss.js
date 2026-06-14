4582f942df24a9c139d4f50667438cb14cefcd9f
El vigilante <cololacalempira5@gmail.com>
2026-06-13 19:40:35 -0600

    Actualizar kiss.js

--- routes/anime/kiss.js
+++ routes/anime/kiss.js
@@ -5,6 +5,15 @@ const videos = [
     'https://files.catbox.moe/9klm6t.mp4',
     'https://files.catbox.moe/0ltn0t.mp4',
     'https://files.catbox.moe/4ghg0x.mp4',
+    'https://files.catbox.moe/q52gel.webp'
+    'https://files.catbox.moe/d6yycp.webp'
+    'https://files.catbox.moe/kgvlr0.webp'
+    'https://files.catbox.moe/lgwsc9.webp'
+    'https://files.catbox.moe/87gm66.webp'
+    'https://files.catbox.moe/cx6v44.webp'
+    'https://files.catbox.moe/r6371n.webp'
+    'https://files.catbox.moe/zfd963.webp'
+    'https://files.catbox.moe/buh0py.webp'
     // Agrega más URLs aquí
 ];
 
